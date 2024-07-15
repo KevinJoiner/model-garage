dba91471db83e7f2b74796d63e8717b9abfb4650
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-07-15 12:00:08 -0400

    Add schema check for soc

--- pkg/vss/convert/vehicle-convert-funcs_gen.go
+++ pkg/vss/convert/vehicle-convert-funcs_gen.go
@@ -297,8 +297,12 @@ func ToPowertrainTractionBatteryGrossCapacity0(originalDoc []byte, val float64) 
 // Vehicle.Powertrain.TractionBattery.StateOfCharge.Current: Physical state of charge of the high voltage battery, relative to net capacity. This is not necessarily the state of charge being displayed to the customer.
 // Unit: 'percent' Min: '0' Max: '100.0'
 func ToPowertrainTractionBatteryStateOfChargeCurrent0(originalDoc []byte, val float64) (float64, error) {
-	// soc comes in as a value between 0 and 1, convert to percentage.
-	return val * 100, nil
+	version := GetSchemaVersion(originalDoc)
+	if semver.Compare(StatusV1, version) == 0 || semver.Compare(StatusV1Converted, version) == 0 {
+		// soc comes in as a value between 0 and 1, convert to percentage.
+		return val * 100, nil
+	}
+	return val, nil
 }
 
 // ToPowertrainTransmissionTravelledDistance0 converts data from field 'odometer' of type float64 to 'Vehicle.Powertrain.Transmission.TravelledDistance' of type float64.
