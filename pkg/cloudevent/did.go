ab7de82b6eb49d0b86530373d2561ba970e07673
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2024-10-07 10:11:49 -0400

    Move DID to cloudevent package

--- pkg/cloudevent/did.go
+++ pkg/cloudevent/did.go
@@ -1,4 +1,4 @@
-package convert
+package cloudevent
 
 import (
 	"errors"
--- pkg/cloudevent/did_test.go
+++ pkg/cloudevent/did_test.go
@@ -1,9 +1,9 @@
-package convert_test
+package cloudevent_test
 
 import (
 	"testing"
 
-	"github.com/DIMO-Network/model-garage/pkg/convert"
+	"github.com/DIMO-Network/model-garage/pkg/cloudevent"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
@@ -12,13 +12,13 @@ func TestDecodeDID(t *testing.T) {
 	tests := []struct {
 		name          string
 		input         string
-		expectedDID   convert.DID
+		expectedDID   cloudevent.DID
 		expectedError bool
 	}{
 		{
 			name:  "valid DID",
 			input: "did:nft:137:0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF_123",
-			expectedDID: convert.DID{
+			expectedDID: cloudevent.DID{
 				ChainID:         "137",
 				ContractAddress: common.HexToAddress("0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF"),
 				TokenID:         123,
@@ -27,32 +27,32 @@ func TestDecodeDID(t *testing.T) {
 		{
 			name:          "invalid format - wrong part count",
 			input:         "did:nft:1",
-			expectedDID:   convert.DID{},
+			expectedDID:   cloudevent.DID{},
 			expectedError: true,
 		},
 		{
 			name:          "invalid format - wrong token part count",
 			input:         "did:nft:1:0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF",
-			expectedDID:   convert.DID{},
+			expectedDID:   cloudevent.DID{},
 			expectedError: true,
 		},
 		{
 			name:          "invalid tokenID",
 			input:         "did:nft:1:0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF_notanumber",
-			expectedDID:   convert.DID{},
+			expectedDID:   cloudevent.DID{},
 			expectedError: true,
 		},
 		{
 			name:          "invalid DID string - wrong prefix",
 			input:         "invalidprefix:nft:1:0xbA5738a18d83D41847dfFbDC6101d37C69c9B0cF_1",
-			expectedDID:   convert.DID{},
+			expectedDID:   cloudevent.DID{},
 			expectedError: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			did, err := convert.DecodeDID(tt.input)
+			did, err := cloudevent.DecodeDID(tt.input)
 
 			// Check if the error matches the expected error
 			if tt.expectedError {
