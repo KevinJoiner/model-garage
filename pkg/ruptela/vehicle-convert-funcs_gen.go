4e234871c36a1ba12ad6dc7aa799e9cb4317bb14
Daniel Bacchus <daniel@dimo.zone>
2024-11-12 15:13:54 -0500

    Add Ad Blue Signals for Ruptela

--- pkg/ruptela/vehicle-convert-funcs_gen.go
+++ pkg/ruptela/vehicle-convert-funcs_gen.go
@@ -118,14 +118,21 @@ func ToOBDRunTime0(originalDoc []byte, val string) (float64, error) {
 // Vehicle.Powertrain.CombustionEngine.DieselExhaustFluid.Capacity: Capacity in liters of the Diesel Exhaust Fluid Tank.
 // Unit: 'l'
 func ToPowertrainCombustionEngineDieselExhaustFluidCapacity0(originalDoc []byte, val string) (float64, error) {
-	panic("not implemented")
+	return Convert107(val)
 }
 
 // ToPowertrainCombustionEngineDieselExhaustFluidCapacity1 converts data from field 'signals.1149' of type string to 'Vehicle.Powertrain.CombustionEngine.DieselExhaustFluid.Capacity' of type float64.
 // Vehicle.Powertrain.CombustionEngine.DieselExhaustFluid.Capacity: Capacity in liters of the Diesel Exhaust Fluid Tank.
 // Unit: 'l'
 func ToPowertrainCombustionEngineDieselExhaustFluidCapacity1(originalDoc []byte, val string) (float64, error) {
-	panic("not implemented")
+	return ignoreZero(Convert107(val))
+}
+
+// ToPowertrainCombustionEngineDieselExhaustFluidLevel0 converts data from field 'signals.1150' of type string to 'Vehicle.Powertrain.CombustionEngine.DieselExhaustFluid.Level' of type float64.
+// Vehicle.Powertrain.CombustionEngine.DieselExhaustFluid.Level: Level of the Diesel Exhaust Fluid tank as percent of capacity. 0 = empty. 100 = full.
+// Unit: 'percent' Min: '0' Max: '100'
+func ToPowertrainCombustionEngineDieselExhaustFluidLevel0(originalDoc []byte, val string) (float64, error) {
+	return Convert107(val)
 }
 
 // ToPowertrainCombustionEngineECT0 converts data from field 'signals.96' of type string to 'Vehicle.Powertrain.CombustionEngine.ECT' of type float64.
--- pkg/ruptela/vehicle-v1-convert_gen.go
+++ pkg/ruptela/vehicle-v1-convert_gen.go
@@ -245,6 +245,22 @@ func SignalsFromV1Data(baseSignal vss.Signal, jsonData []byte) ([]vss.Signal, []
 		retSignals = append(retSignals, sig)
 	}
 
+	val, err = PowertrainCombustionEngineDieselExhaustFluidLevelFromV1Data(jsonData)
+	if err != nil {
+		if !errors.Is(err, errNotFound) {
+			errs = append(errs, fmt.Errorf("failed to get 'PowertrainCombustionEngineDieselExhaustFluidLevel': %w", err))
+		}
+	} else {
+		sig := vss.Signal{
+			Name:      "powertrainCombustionEngineDieselExhaustFluidLevel",
+			TokenID:   baseSignal.TokenID,
+			Timestamp: baseSignal.Timestamp,
+			Source:    baseSignal.Source,
+		}
+		sig.SetValue(val)
+		retSignals = append(retSignals, sig)
+	}
+
 	val, err = PowertrainCombustionEngineECTFromV1Data(jsonData)
 	if err != nil {
 		if !errors.Is(err, errNotFound) {
@@ -797,6 +813,31 @@ func PowertrainCombustionEngineDieselExhaustFluidCapacityFromV1Data(jsonData []b
 
 	if errs == nil {
 		return ret, fmt.Errorf("%w 'PowertrainCombustionEngineDieselExhaustFluidCapacity'", errNotFound)
+	}
+
+	return ret, errs
+}
+
+// PowertrainCombustionEngineDieselExhaustFluidLevelFromV1Data converts the given JSON data to a float64.
+func PowertrainCombustionEngineDieselExhaustFluidLevelFromV1Data(jsonData []byte) (ret float64, err error) {
+	var errs error
+	var result gjson.Result
+	result = gjson.GetBytes(jsonData, "data.signals.1150")
+	if result.Exists() && result.Value() != nil {
+		val, ok := result.Value().(string)
+		if ok {
+			retVal, err := ToPowertrainCombustionEngineDieselExhaustFluidLevel0(jsonData, val)
+			if err == nil {
+				return retVal, nil
+			}
+			errs = errors.Join(errs, fmt.Errorf("failed to convert 'data.signals.1150': %w", err))
+		} else {
+			errs = errors.Join(errs, fmt.Errorf("%w, field 'data.signals.1150' is not of type 'string' got '%v' of type '%T'", convert.InvalidTypeError(), result.Value(), result.Value()))
+		}
+	}
+
+	if errs == nil {
+		return ret, fmt.Errorf("%w 'PowertrainCombustionEngineDieselExhaustFluidLevel'", errNotFound)
 	}
 
 	return ret, errs
