03d9575746f666ab8c6f9ccdd4ef3cc2e958ab8c
dmytrozakharenko <amidum@gmail.com>
2024-11-27 15:12:15 -0500

    - added ConvertToCloudEvents func

--- pkg/autopi/status.go
+++ pkg/autopi/status.go
@@ -2,22 +2,40 @@
 package autopi
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/DIMO-Network/model-garage/pkg/cloudevent"
 	"github.com/DIMO-Network/model-garage/pkg/convert"
+	"github.com/segmentio/ksuid"
 	"github.com/tidwall/gjson"
 	"golang.org/x/mod/semver"
 )
+
+const (
+	StatusEventType      = "com.dimo.device.status.v2"
+	FingerprintEventType = "zone.dimo.aftermarket.device.fingerprint"
+	DataVersion          = "v2"
+)
+
+type AutopiEvent struct {
+	Data           json.RawMessage `json:"data"`
+	VehicleTokenID *uint64         `json:"vehicleTokenId"`
+	DeviceTokenID  *uint32         `json:"deviceTokenId"`
+	Signature      string          `json:"signature"`
+	Time           string          `json:"time"`
+	Type           string          `json:"type"`
+}
 
 const (
 	// StatusV1 is the version string for payloads with the version 1.0 schema.
 	StatusV1 = "v1.0.0"
 	// StatusV1Converted is the version string for payloads that have been converted to the 1.0 schema.
 	StatusV1Converted = "v1.1.0"
-	// StatusV2 is the version string for payloads with the version 2.0 schema.
-	StatusV2 = "v2.0.0"
 )
 
 // GetDataVersion returns the version string used in the payload.
@@ -64,4 +82,105 @@ func SourceFromData(jsonData []byte) (string, error) {
 		return "", errors.New("source field is not a string")
 	}
 	return src, nil
+}
+
+// ConvertToCloudEvents converts a message data payload into a slice of CloudEvents.
+// It handles both status and fingerprint events, creating separate CloudEvents for each.
+func ConvertToCloudEvents(msgData []byte, chainID uint64, aftermarketContractAddr, vehicleContractAddr string) ([][]byte, error) {
+	var result [][]byte
+
+	var event AutopiEvent
+	err := json.Unmarshal(msgData, &event)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal record data: %w", err)
+	}
+	if event.DeviceTokenID == nil {
+		return nil, fmt.Errorf("device token id is missing")
+	}
+
+	// handle both status and fingerprint events
+	var eventType string
+	if event.Type == StatusEventType {
+		eventType = cloudevent.TypeStatus
+	} else if event.Type == FingerprintEventType {
+		eventType = cloudevent.TypeFingerprint
+	} else {
+		return nil, fmt.Errorf("unknown event type: %s", event.Type)
+	}
+
+	// Construct the producer DID
+	producer := cloudevent.NFTDID{
+		ChainID:         chainID,
+		ContractAddress: common.HexToAddress(aftermarketContractAddr),
+		TokenID:         *event.DeviceTokenID,
+	}.String()
+
+	// Construct the subject
+	var subject string
+	if event.VehicleTokenID != nil {
+		subject = cloudevent.NFTDID{
+			ChainID:         chainID,
+			ContractAddress: common.HexToAddress(vehicleContractAddr),
+			TokenID:         uint32(*event.VehicleTokenID),
+		}.String()
+	}
+
+	cloudEvent, err := convertToCloudEvent(event, producer, subject, eventType)
+	if err != nil {
+		return nil, err
+	}
+	// Append the status event to the result
+	result = append(result, cloudEvent)
+
+	// Each AP payload has device information, so we need to create separate status event where subject == producer
+	cloudEventDevice, err := convertToCloudEvent(event, producer, producer, cloudevent.TypeStatus)
+	if err != nil {
+		return nil, err
+	}
+
+	// Append the status event to the result
+	result = append(result, cloudEventDevice)
+
+	return result, nil
+}
+
+// convertToCloudEvent wraps an AutopiEvent into a CloudEvent.
+// Returns:
+//   - A byte slice containing the JSON representation of the CloudEvent.
+//   - An error if the CloudEvent creation or marshaling fails.
+func convertToCloudEvent(event AutopiEvent, producer, subject, eventType string) ([]byte, error) {
+	cloudEvent, err := createCloudEvent(event, producer, subject, eventType)
+	if err != nil {
+		return nil, err
+	}
+
+	cloudEventBytes, err := json.Marshal(cloudEvent)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal cloudEvent: %w", err)
+	}
+	return cloudEventBytes, nil
+}
+
+// createCloudEvent creates a cloud event from autopi event.
+func createCloudEvent(event AutopiEvent, producer, subject, eventType string) (cloudevent.CloudEvent[json.RawMessage], error) {
+	timeValue, err := time.Parse(time.RFC3339, event.Time)
+	if err != nil {
+		return cloudevent.CloudEvent[json.RawMessage]{}, fmt.Errorf("Failed to parse time: %v\n", err)
+	}
+	return cloudevent.CloudEvent[json.RawMessage]{
+		CloudEventHeader: cloudevent.CloudEventHeader{
+			DataContentType: "application/json",
+			ID:              ksuid.New().String(),
+			Subject:         subject,
+			SpecVersion:     "1.0",
+			Time:            timeValue,
+			Type:            eventType,
+			DataVersion:     DataVersion,
+			Producer:        producer,
+			Extras: map[string]any{
+				"signature": event.Signature,
+			},
+		},
+		Data: event.Data,
+	}, nil
 }
